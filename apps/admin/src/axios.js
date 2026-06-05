d144b70d361f4e2e4eaa389523a1dd5d316bd598
angkeinfo <angkeinfo@gmail.com>
2026-06-05 10:33:40 +0800

    sha256 自己寫

--- apps/admin/src/axios.js
+++ apps/admin/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosClient = axios.create({
-  baseURL: import.meta.env.DEV ? '/api' : `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: `/api`,
   withCredentials: true,
   timeout: 600000,
   headers: {
